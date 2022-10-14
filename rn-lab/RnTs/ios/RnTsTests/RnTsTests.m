7ebc384b2fe67dce7fad573ff74ae5d53c5974b0
fuRan <NgeKaworu@gmail.com>
2022-10-14 14:44:33 +0800

    init: rn-demo

--- rn-lab/RnTs/ios/RnTsTests/RnTsTests.m
+++ rn-lab/RnTs/ios/RnTsTests/RnTsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RnTsTests : XCTestCase
+@interface rntsTests : XCTestCase
 
 @end
 
-@implementation RnTsTests
+@implementation rntsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
